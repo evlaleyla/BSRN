3b14bbe4114d349cf537ec428c393529581d81c9
aminekln <136360004+aminekln@users.noreply.github.com>
2023-06-23 16:17:21 +0200

    Update uffjunge.c

--- uffjunge.c
+++ uffjunge.c
@@ -20,6 +20,7 @@ void start_daemon()
     if (pid < 0)
     {
         perror("Fehler beim Starten des Daemons\n");
+        syslog(LOG_ERR, "Fehler beim Starten des Daemons");
         exit(1);
     }
     
@@ -32,16 +33,20 @@ void start_daemon()
         if (session < 0)
         {
             perror("Fehler beim Erstellen einer neuen Sitzung\n");
+            syslog(LOG_ERR, "Fehler beim Erstellen einer neuen Sitzung");
+            
             exit(1);
         }
         if (session >= 0)
         {
-            printf("Neue Session wurde erstellt. SessionnID %d\n", session);
+            printf("Neue Session wurde erstellt. Die Session-ID %d\n", session);
+            syslog(LOG_INFO, "Neue Session wurde erstellt.");
         }
                char *arguments[] = {"/home/safashg/Schreibtisch/Projekt/funktionierdochendlich.c", NULL};
        
         if (execvp("/home/safashg/Schreibtisch/Projekt/helloworld", arguments) < 0){
           perror("Fehler beim Ausführen des Programms\n");
+         syslog(LOG_ERR, "Fehler beim Ausführen des Programms");
           exit(1);
        }
 
@@ -54,6 +59,7 @@ void start_daemon()
     if (chdir("/") < 0)
     {
         perror("Fehler beim Verzeichniswechsel\n");
+        syslog(LOG_ERR, "Fehler beim Verzeichniswechsel");
         exit(1);
     }
 
@@ -61,13 +67,16 @@ void start_daemon()
     if (getuid() == 0)
     {
         printf("Das Programm wird bereits mit Superuser-Rechten ausgeführt.\n");
+        syslog(LOG_INFO, "Das Programm wird bereits mit Superuser-Rechten ausgeführt.");
     }
     if (setuid(0) == 0)
     {
         printf("Superuser-Rechte erfolgreich erhalten.\n");
+        syslog(LOG_INFO, "Superuser-Rechte erfolgreich erhalten.");
+        
     }
 
-    umask(0); // Setze die Zugriffsrechte für Dateien
+    umask(777); // Setze die Zugriffsrechte für Dateien
 
 
     time_t rawtime;
@@ -79,8 +88,7 @@ void start_daemon()
 
     strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
     printf("Daemon wurde gestartet um: %s\n", buffer);
-
-     syslog(LOG_INFO, "Daemon wurde gestartet um: %s\n", buffer );
+    syslog(LOG_INFO, "Daemon wurde gestartet um: %s\n", buffer );
 }
 
 
@@ -90,13 +98,14 @@ void create_pid_file()
     if (!pid_file)
     {
         perror("Fehler beim Erstellen der PID-Datei\n");
+        syslog(LOG_ERR, "Fehler beim Erstellen der PID-Datei");
         exit(1);
     } else {
     fprintf(pid_file, "%d", getpid()); // Schreibe die PID des aktuellen Prozesses in die Datei
     
     }
     fclose(pid_file);
-syslog(LOG_INFO, "Datei wurde erstellt.");
+    syslog(LOG_INFO, "Datei wurde erstellt.");
 
 
 }
@@ -107,6 +116,7 @@ void check_daemon_status()
     if (!pid_file)
     {
         perror("Fehler beim Lesen der PID-Datei\n");
+        syslog(LOG_ERR, "Fehler beim Lesen der PID-Datei");
         exit(1);
     }
     pid_t pid;
@@ -115,10 +125,12 @@ void check_daemon_status()
     if (kill(pid, 0) == 0)
     {
         printf("Daemon läuft (PID: %d)\n", pid);
+        syslog(LOG_INFO, "Daemon läuft");
     }
     else
     {
         printf("Daemon ist nicht aktiv\n");
+        syslog(LOG_INFO, "Daemon ist nicht aktiv");
         exit(1);
     }
 }
@@ -142,6 +154,7 @@ struct ProzessInfo get_process_info(pid_t pid)
     if (!statm_file)
     {
         perror("Fehler beim Lesen der Prozessinformationen\n");
+        syslog(LOG_ERR, "Fehler beim Lesen der Prozessinformationen");
         exit(1);
     }
 
@@ -156,6 +169,7 @@ struct ProzessInfo get_process_info(pid_t pid)
     if (!stat_file)
     {
         perror("Fehler beim Lesen der Prozessinformationen\n");
+        syslog(LOG_ERR,"Fehler beim Lesen der Prozessinformationen");
         exit(1);
     }
 
@@ -178,6 +192,8 @@ void ausgabe(){
     
     if (sysinfo(&sys_info) != 0) {
         printf("Fehler beim Abrufen der Systeminformationen.\n");
+        syslog(LOG_ERR,"Fehler beim Abrufen der Systeminformationen.");
+        
         exit(1);
     }
     
@@ -207,7 +223,7 @@ void ausgabe(){
 
       int daemonInformationen;
 
-    printf("Möchten Sie Informationen erhalten? Wenn ja, dann tippe 1. wenn nein dann 0\n");
+    printf("Möchten Sie Informationen erhalten? Wenn Ja, dann tippen Sie bitte '1', wenn Nein, dann tippen Sie '0'.\n");
         scanf("%d", &daemonInformationen);
 
         
@@ -217,6 +233,7 @@ void ausgabe(){
             FILE *pid_file = fopen("/home/safashg/Schreibtisch/Projekt/log.txt", "r");
             if (!pid_file){
                 printf("Fehler beim Öffnen der Datei.\n");
+                syslog(LOG_ERR,"Fehler beim Öffnen der Datei.");
                 exit(1);
             }
             char puffer[256]; //angenommene maxiale zeilenlänge
@@ -234,7 +251,6 @@ void ausgabe(){
 
 void run_daemon()
 {  
-    printf("hallo"); 
     int running = 1;
     int daemonBeenden;
     time_t rawtime;
@@ -249,26 +265,26 @@ void run_daemon()
    
     while (running)
     {
-        printf("1");
-        syslog(LOG_INFO, "Daemon läuft...");
-        syslog(LOG_ERR, "Fehler beim Verarbeiten der Anfrage.");
+    
         sleep(5); // Warte für 5 Sekunden
         syslog(LOG_INFO, "Daemon schläft...");
         
 
         // Überprüfe, ob der Daemon beendet werden soll
-        printf("Moechten Sie den Daemon beenden? wenn ja dann 1");
+        printf("Moechten Sie den Daemon beenden? Wenn Sie ihn beenden möchten, drücken Sie die 1\n");
         scanf("%d", &daemonBeenden);
         if (daemonBeenden == 1)
         {
-            printf("2");
+     
             printf("Daemon wird gestoppt..");
-           printf("3");
+            syslog(LOG_INFO, "Daemon wird gestoppt..");
+       
     
     FILE *pid_file = fopen("/home/safashg/Schreibtisch/Projekt/log.txt", "r");
    
    if(!pid_file){
     perror("Fehler beim lesen der PID-Datei\n");
+    syslog(LOG_ERR, "Fehler beim lesen der PID-Datei");
     exit(1);
    }
 
@@ -286,10 +302,12 @@ void run_daemon()
    
     }else {
     perror("Fehler beim Beenden des Daemons\n");
+    syslog(LOG_ERR, "Fehler beim Beenden des Daemons");
         exit(1);
     }       
         } else if(daemonBeenden == 0){
-            printf("daemon läuft weiter");
+            printf("Der Daemon läuft ordnungsgemäß weiter.\n");
+            syslog(LOG_INFO, "Der Daemon läuft ordnungsgemäß weiter.");
         }
         }
     }
@@ -308,26 +326,25 @@ int main()
 
 
     setlogmask(LOG_UPTO(LOG_NOTICE));
-    openlog("daemons", LOG_PID, LOG_DAEMON); // Öffne das Syslog für den Daemon-Prozess
+    openlog("Daemon", LOG_PID, LOG_DAEMON); // Öffne das Syslog für den Daemon-Prozess
    
    /* if(syslog(LOG_ERR, "Fehler beim öffnen der Datei") < 0){
         perror("Fehler beim protokollieren der Meldung\n");
         exit(1);
     }*/
-    printf("Wollen Sie einen Daemon starten?\nGeben Sie 1 ein, damit ein Daemon gestartet wird.\nGeben Sie 0 ein damit kein Daemon gestartet wird.");
+    printf("Wollen Sie einen Daemon starten?\nGeben Sie bitte '1' ein, damit ein Daemon gestartet wird.\nGeben Sie bitte '0' ein, damit kein Daemon gestartet wird.");
     scanf("%d", &daemonStart);
     switch (daemonStart)
     {
     case 1:
         create_pid_file();
         start_daemon();
-        printf("Daemon gestartet.\n");
         ausgabe();
         run_daemon();
       
         break;
     case 0:
-        printf("Eingabe gespeichert. Daemon wird nicht gestartet.");
+        printf("Eingabe gespeichert. Der Daemon wird nicht gestartet.\n");
         break;
     default:
         printf("Falsche Eingabe");
